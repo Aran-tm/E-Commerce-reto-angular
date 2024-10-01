5e513c88a5eda942d7dbe9e791f65740b3f31bc4
Armando Roberto Travieso Montes <armandortmontes@gmail.com>
2024-10-01 09:25:33 -0500

    Finishing sign in and sign up mockups

--- src/app/pages/auth/components/sign-up/sign-up.component.ts
+++ src/app/pages/auth/components/sign-up/sign-up.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
 })
-export class SignUpComponent {
-
-}
+export class SignUpComponent {}
