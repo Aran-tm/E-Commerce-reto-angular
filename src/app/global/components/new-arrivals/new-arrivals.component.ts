e87a8cc5934955f3f39dfe5c5c75a230d3a547bf
Armando Roberto Travieso Montes <armandortmontes@gmail.com>
2024-10-02 08:20:06 -0500

    Banner grid finished

---
+++ src/app/global/components/new-arrivals/new-arrivals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-arrivals',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-arrivals.component.html',
+  styles: ``,
+})
+export class NewArrivalsComponent {}
